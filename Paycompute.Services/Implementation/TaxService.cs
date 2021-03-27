854e4af7d76c88fbf0700497be13d9a4ea6255f1
Aman Verma <aman.vr@outlook.com>
2021-03-27 12:10:24 +0530

    TaxService Interface and Implementation

---
+++ Paycompute.Services/Implementation/TaxService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Paycompute.Services.Implementation
+{
+    public class TaxService : ITaxService
+    {
+        public decimal taxRate;
+        public decimal tax;
+        public decimal TaxAmount(decimal totalAmount)
+        {
+            if(totalAmount<=1042)
+            {
+                taxRate = 0.0m;
+                tax = totalAmount * taxRate;
+            }
+            else if(totalAmount <= 3125)
+            {
+                taxRate = 0.2m;
+                tax = (totalAmount - 1042) * taxRate;
+            }
+            else if(totalAmount<=12500)
+            {
+                taxRate = 0.40m;
+                tax = ((3125 - 1042) * 0.2m) + ((totalAmount - 3125) * taxRate);
+            }
+            else
+            {
+                taxRate = 0.45m;
+                tax = ((3125 - 1042) * 0.2m) + ((12500 - 3125) * 0.4m) + ((totalAmount-12500) *0.45m);
+            }
+            return tax;
+        }
+    }
+}
