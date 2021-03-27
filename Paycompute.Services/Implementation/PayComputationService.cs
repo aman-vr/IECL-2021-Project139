d2bbf243b856011c9a9b70c69ca9b2aead5a6895
Aman Verma <aman.vr@outlook.com>
2021-03-27 10:36:22 +0530

    Paycomputation Interface and Implementation

---
+++ Paycompute.Services/Implementation/PayComputationService.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Paycompute.Entity;
+using Paycompute.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paycompute.Services.Implementation
+{
+    public class PayComputationService : IPayComputationService
+    {
+        private readonly ApplicationDbContext _context;
+        private decimal contractualEarnings;
+        private decimal overtimeHours;
+        public PayComputationService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public decimal ContractualEarnings(decimal contractualHours, decimal hoursWorked, decimal hourlyRate)
+        {
+            return contractualEarnings = (hoursWorked >= contractualHours) ? (hourlyRate * contractualHours) : (hourlyRate * hoursWorked);
+            //if(hoursWorked >= contractualHours)
+            //  contractualEarnings = hourlyRate * contractualHours;
+            //else
+            //  contractualEarnings = hourlyRate * hoursWorked;
+            //return contractualEarnings;
+        }
+
+        public async Task CreateAsync(PaymentRecord paymentRecord)
+        {
+            await _context.PaymentRecords.AddAsync(paymentRecord);
+            await _context.SaveChangesAsync();
+        }
+
+        public IEnumerable<PaymentRecord> GetAll() 
+            => _context.PaymentRecords.OrderBy(p => p.EmployeeId);
+
+        public IEnumerable<SelectListItem> GetAllTaxYear()
+        {
+            var allTaxYear = _context.TaxYears.Select(taxYears => new SelectListItem 
+            { 
+                Text = taxYears.YearOfTax,
+                Value = taxYears.Id.ToString()
+            });
+            return allTaxYear;
+        }
+
+        public PaymentRecord GetById(int id) 
+            => _context.PaymentRecords.Where(pay => pay.Id == id).FirstOrDefault();
+
+        public decimal NetPay(decimal totalEarnings, decimal totalDeduction) 
+            => totalEarnings - totalDeduction;
+
+        public decimal OvertimeEarnings(decimal overtimeRate, decimal overTimeHours) 
+            => overTimeHours * overtimeRate;
+        public decimal OvertimeHours(decimal hoursWorked, decimal contractualHours) 
+            => overtimeHours = (hoursWorked > contractualHours) ? (hoursWorked - contractualHours) : (0.00m);
+
+        public decimal OvertimeRate(decimal hourlyRate) 
+            => hourlyRate * 1.5m;
+
+        public decimal TotalDeduction(decimal tax, decimal nic, decimal studentLoanRepayment, decimal unionFees)
+            => tax + nic + studentLoanRepayment + unionFees;
+
+        public decimal TotalEarnings(decimal overtimeEarnings, decimal contractualEarnings)
+            => overtimeEarnings + contractualEarnings;
+    }
+}
--- Paycompute/Models/EmployeeCreateViewModel.cs
+++ Paycompute/Models/EmployeeCreateViewModel.cs
@@ -12,7 +12,7 @@ namespace Paycompute.Models
     {
         public int Id { get; set; }
         [Required(ErrorMessage = "Employee Number is required"),
-            RegularExpression(@"^[A-Z]{3-3}[0-9]{3}$")]
+            RegularExpression(@"^[A-Z]{3,3}[0-9]{3}$")]
         public string EmployeeNo { get; set; }
         [Required(ErrorMessage = "First Name is required"), StringLength(50, MinimumLength = 2),
             RegularExpression(@"^[A-Z][a-zA-Z""\s-]*$"), Display(Name ="First Name")]
@@ -26,7 +26,7 @@ namespace Paycompute.Models
         public string FullName {
             get
             {
-                return FirstName + (string.IsNullOrEmpty(MiddleName) ? " " : (" " + (char)MiddleName[0] + ".")).ToUpper() + LastName;
+                return FirstName + (string.IsNullOrEmpty(MiddleName) ? " " : (" " + (char)MiddleName[0] + ". ")).ToUpper() + LastName;
             }
         }
         public string Gender { get; set; }
@@ -43,7 +43,7 @@ namespace Paycompute.Models
         public string Email { get; set; }
 
         [Required, StringLength(50), Display(Name ="NI No.")]
-        [RegularExpression(@"^[A-CEGHJ-PR-TW-Z]{1}[A-CEGHJ-NPR-TW-Z]{1}[0-9]{6}[A-D]\s$")]
+        [RegularExpression(@"^[A-CEGHJ-PR-TW-Z]{1}[A-CEGHJ-NPR-TW-Z]{1}[0-9]{6}[A-D\s]$")]
         public string NationalInsuranceNo { get; set; }
         [Display(Name="Payment Method")]
         public PaymentMethod PaymentMethod { get; set; }
